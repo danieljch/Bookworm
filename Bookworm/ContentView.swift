303099e6aeed25f896d021aba00f3d89d69312b4
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-01 23:21:32 -0400

    first binding example

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rememberMe = false
+
     var body: some View {
-        Text("Hello, world!")
+        Toggle("Remember Me", isOn: $rememberMe)
+    }
+    struct PushButton: View {
+        let title: String
+        @State var isOn: Bool
+
+        var onColors = [Color.red, Color.yellow]
+        var offColors = [Color(white: 0.6), Color(white: 0.4)]
+
+        var body: some View {
+            Button(title) {
+                isOn.toggle()
+            }
             .padding()
+            .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+            .foregroundColor(.white)
+            .clipShape(Capsule())
+            .shadow(radius: isOn ? 0 : 5)
+        }
     }
 }
 
