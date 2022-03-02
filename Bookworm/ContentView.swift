15faafc64556a12d83acf0c2667c2be550e5136c
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-02 09:28:25 -0400

    text editor example

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,34 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
 
     var body: some View {
-        VStack {
-                   PushButton(title: "Remember Me", isOn: $rememberMe)
-                   Text(rememberMe ? "On" : "Off")
-               }
-    }
-    struct PushButton: View {
-        let title: String
-        @Binding var isOn: Bool
-
-        var onColors = [Color.red, Color.yellow]
-        var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-        var body: some View {
-            Button(title) {
-                isOn.toggle()
-            }
-            .padding()
-            .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
-            .shadow(radius: isOn ? 0 : 5)
+        NavigationView {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
